7ee97e2320a8f0d38e0b57628281a5b1a01f1b7a
Gabrieldamasceno <juragabriel21@gmail.com>
2025-03-22 17:05:12 -0300

    att

---
+++ Operadores/Operadores/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 10;
+
+            int resultado = num1 + num2;
+            Console.WriteLine("Resultado da soma: " + resultado);
+        }
+    }
+}
