491cfea2aa1543b366e0c6ce34f62c865897f4a1
Gabrieldamasceno <juragabriel21@gmail.com>
2025-03-22 10:50:24 -0300

    att

--- Variaveis/Variaveis/Program.cs
+++ Variaveis/Variaveis/Program.cs
@@ -16,6 +16,13 @@ namespace Variaveis
         sexta,
         sabado
     }
+
+    struct pessoa
+    {
+        public string nome;
+        public int idade;
+        public double altura;
+    }   
     internal class Program
     {
         static void Main(string[] args)
@@ -50,36 +57,56 @@ namespace Variaveis
             #endregion
 
             #region Enum
-            diaSemana dia = diaSemana.segunda;
+            //diaSemana dia = diaSemana.segunda;
 
-            switch (dia)
-            {
-                case diaSemana.domingo:
-                    Console.WriteLine("Domingo");
-                    break;
-                case diaSemana.segunda:
-                    Console.WriteLine("Segunda");
-                    break;
-                case diaSemana.terca:
-                    Console.WriteLine("Terça");
-                    break;
-                case diaSemana.quarta:
-                    Console.WriteLine("Quarta");
-                    break;
-                case diaSemana.quinta:
-                    Console.WriteLine("Quinta");
-                    break;
-                case diaSemana.sexta:
-                    Console.WriteLine("Sexta");
-                    break;
-                case diaSemana.sabado:
-                    Console.WriteLine("Sábado");
-                    break;
-                default:
-                    break;
-            }
+            //switch (dia)
+            //{
+            //    case diaSemana.domingo:
+            //        Console.WriteLine("Domingo");
+            //        break;
+            //    case diaSemana.segunda:
+            //        Console.WriteLine("Segunda");
+            //        break;
+            //    case diaSemana.terca:
+            //        Console.WriteLine("Terça");
+            //        break;
+            //    case diaSemana.quarta:
+            //        Console.WriteLine("Quarta");
+            //        break;
+            //    case diaSemana.quinta:
+            //        Console.WriteLine("Quinta");
+            //        break;
+            //    case diaSemana.sexta:
+            //        Console.WriteLine("Sexta");
+            //        break;
+            //    case diaSemana.sabado:
+            //        Console.WriteLine("Sábado");
+            //        break;
+            //    default:
+            //        break;
+            //}
             #endregion
 
+            pessoa p1 = new pessoa();
+            p1.nome = "Gabriel";
+            p1.idade = 18;
+            p1.altura = 1.70;
+
+            pessoa p2 = new pessoa();
+            p2.nome= "Maria";
+            p2.idade = 44;
+            p2.altura = 1.55;
+
+
+            Console.WriteLine("Nome: " + p1.nome);
+            Console.WriteLine("Idade: " + p1.idade);
+            Console.WriteLine("Altura: " + p1.altura);
+
+            Console.WriteLine("====================================");
+
+            Console.WriteLine("Nome: " + p2.nome);
+            Console.WriteLine("Idade: " + p2.idade);
+            Console.WriteLine("Altura: " + p2.altura);
 
             Console.ReadKey();
         }
