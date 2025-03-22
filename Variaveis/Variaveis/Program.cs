c65a9f0f744012cab7be753dc9f7067f9498df28
Gabrieldamasceno <juragabriel21@gmail.com>
2025-03-22 10:28:30 -0300

    att

--- Variaveis/Variaveis/Program.cs
+++ Variaveis/Variaveis/Program.cs
@@ -6,49 +6,81 @@ using System.Threading.Tasks;
 
 namespace Variaveis
 {
-    enum notas
+    enum diaSemana
     {
-        minima,
-        media,
-        maxima
+        domingo,
+        segunda,
+        terca,
+        quarta,
+        quinta,
+        sexta,
+        sabado
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            //#region números integrais
+            #region números integrais
             //int numero;
             //numero = 100;
 
             //numero = 120;
-            //#endregion
+            #endregion
 
-            //#region números decimais
+            #region números decimais
             //double num1;
 
             //num1 = 100.32;
-            //#endregion
+            #endregion
 
-            //#region Char
+            #region Char
 
             //char letra = 'g';
 
-            //#endregion
+            #endregion
 
-            //#region Boolean
+            #region Boolean
             //bool verificar;
             //verificar = false;
-            //#endregion
+            #endregion
 
-            //#region String
+            #region String
             //string nome;
             //nome = Console.ReadLine();
-            //#endregion
+            #endregion
 
-            notas notaAluno = notas.media;
-            notaAluno = 
+            #region Enum
+            diaSemana dia = diaSemana.segunda;
 
-            Console.WriteLine("Seu nome é: ");
+            switch (dia)
+            {
+                case diaSemana.domingo:
+                    Console.WriteLine("Domingo");
+                    break;
+                case diaSemana.segunda:
+                    Console.WriteLine("Segunda");
+                    break;
+                case diaSemana.terca:
+                    Console.WriteLine("Terça");
+                    break;
+                case diaSemana.quarta:
+                    Console.WriteLine("Quarta");
+                    break;
+                case diaSemana.quinta:
+                    Console.WriteLine("Quinta");
+                    break;
+                case diaSemana.sexta:
+                    Console.WriteLine("Sexta");
+                    break;
+                case diaSemana.sabado:
+                    Console.WriteLine("Sábado");
+                    break;
+                default:
+                    break;
+            }
+            #endregion
+
+
             Console.ReadKey();
         }
     }
