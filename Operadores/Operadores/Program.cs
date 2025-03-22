c0cf405054b1b2136395077c4f18954a73387eec
Gabrieldamasceno <juragabriel21@gmail.com>
2025-03-22 17:38:28 -0300

    att

--- Operadores/Operadores/Program.cs
+++ Operadores/Operadores/Program.cs
@@ -11,10 +11,29 @@ namespace Operadores
         static void Main(string[] args)
         {
             int num1 = 10;
-            int num2 = 10;
+            string num2 = "10";
+            int num3 = 2;
 
-            int resultado = num1 + num2;
-            Console.WriteLine("Resultado da soma: " + resultado);
+            int num2Int = Convert.ToInt32(num2);
+            int somaNums = num1 + num2Int; // Somando os valores
+            int subtracaoNums = num1 - num2Int; // Subtraindo os valores
+            int multiplicacaoNums = num1 * num2Int; // Multiplicando os valores
+            int divisaoNums = num1 / num2Int; // Dividindo os valores
+            int restoDivisaoNums = num1 % num2Int; // Resto da divisão
+
+
+            // Incremento e decremento
+            int num4 = 10;
+            //num4++; // Incremento out = 11
+            //num4--; // Decremento out = 9
+            //++num4; // Pré Incremento  out = 11
+            //--num4; // Pré Decremento out = 9
+
+            Console.WriteLine(num4++); // Imprime valor 10 e depois incrementa o valor totalizando = 11
+            Console.WriteLine(num4); // Imprime valor 11
+
+            Console.WriteLine(++num4); // Incrementa o valor totalizando = 12 e depois imprime o valor
+            Console.WriteLine(--num4); // Decrementa o valor totalizando = 11 e depois imprime o valor
         }
     }
 }
