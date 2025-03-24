bea00881835bc97d1002734c0bde6823a7008bfd
Gabrieldamasceno <juragabriel21@gmail.com>
2025-03-24 16:26:59 -0300

    att

---
+++ Controle/Controle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite algum número entre 1 e 3: ");
+            int escolha = int.Parse(Console.ReadLine());
+
+            switch(escolha)
+            {
+                case 1:
+                    Console.WriteLine("Escolha 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Escolha 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Escolha 3");
+                    break;
+                default:
+                    Console.WriteLine("Nenhuma escolha válida");
+                    break;
+            }
+        }
+    }
+}
