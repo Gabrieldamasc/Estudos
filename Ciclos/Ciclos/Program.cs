788f40cf8fdd3b914c3d8c0351de2b359ff350ef
Gabrieldamasceno <juragabriel21@gmail.com>
2025-03-24 17:36:59 -0300

    att

--- Ciclos/Ciclos/Program.cs
+++ Ciclos/Ciclos/Program.cs
@@ -12,9 +12,26 @@ namespace Ciclos
         {
             int valor = 0;
 
-            while(valor < 10)
+            //while(valor < 10)
+            //{
+            //    Console.WriteLine("O valor atual é: " + valor++);
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine("O Valor atual é: " + valor++);
+            //} while(valor < 15);
+
+            //for(int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("O valor atual é: " + i);
+            //}
+
+            string[] nomes = {"Gabriel", "Maria", "Ana", "João", "Marcos"};
+
+            foreach(var nome in nomes)
             {
-                Console.WriteLine("O valor atual é: " + valor++);
+                Console.WriteLine("O nome atual é: " + nome);
             }
         }
     }
