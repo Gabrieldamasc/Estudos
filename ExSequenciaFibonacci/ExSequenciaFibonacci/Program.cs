f0c45904235670315ed64673d714603675022e6f
Gabrieldamasceno <juragabriel21@gmail.com>
2025-03-24 17:57:11 -0300

    Excercício da criação de algoritmo para representar a sequência de fibonacci

---
+++ ExSequenciaFibonacci/ExSequenciaFibonacci/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExSequenciaFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========= Sequência de fibonacci =========");
+            Console.Write("Quantos números você deseja na sequência: ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            int a = 0, b = 1, c = 0;
+
+            for(int i = 0; i < userInput; i++) 
+            {
+                if(i < userInput - 1)
+                {
+                    Console.Write(a + ", ");
+                }
+                else
+                {
+                    Console.Write(a + ".");
+                }
+               
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
