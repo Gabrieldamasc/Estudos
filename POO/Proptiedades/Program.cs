52561855d730473a8235b81aa954e827d7cbaffb
Gabrieldamasceno <juragabriel21@gmail.com>
2025-03-27 14:06:08 -0300

    att

---
+++ POO/Proptiedades/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proptiedades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Teste p1 = new Teste();
+
+            p1.Idade = int.Parse(Console.ReadLine());
+            p1.Nome = Console.ReadLine();
+
+            Console.WriteLine("Seu nome é: " + p1.Nome + " e você tem " + p1.Idade + " anos");
+
+        }
+    }
+}
---
+++ POO/Proptiedades/Teste.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proptiedades
+{
+    class Teste
+    {
+        private int idade;
+        private string nome;
+
+        public int Idade
+        {
+            get
+            {
+                return idade;
+            }
+
+            set
+            {
+                idade = value;
+            }
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+            }
+        }
+    }
+}
